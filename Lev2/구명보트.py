bb431c10bf6875d72925e701b7519e84a8499868
이기태 <91439717+kitaee@users.noreply.github.com>
2022-06-30 16:17:39 +0900

    Create 구명보트.py

---
+++ Lev2/구명보트.py
@@ -0,0 +1,17 @@
+def solution(people, limit):
+    answer = 0
+    startIndex = 0
+    endIndex = len(people)-1
+    people.sort()
+    
+    while(startIndex<endIndex):
+        if people[startIndex] + people[endIndex] <= limit:
+            startIndex+=1
+            endIndex-=1
+        else:
+            endIndex-=1
+        answer+=1
+    if startIndex>endIndex:
+        return answer
+    else:
+        return answer+1
